ec2a869dfc842bbca976a92846bebfd1eb3a7458
Your Name <your.email@example.com>
2025-06-15 12:15:47 +0100

    Updates

---
+++ assets/CardContent-DjNJdOrj.js
@@ -0,0 +1,1 @@
+import{c as r}from"./useToast-D-yC0XMS.js";import{d as t,o,r as n,n as c,u as l}from"./main-MNcEe9nS.js";const d={__name:"CardContent",props:{class:{type:String,default:""}},setup(s){const a=s;return(e,p)=>(o(),t("div",{class:c(l(r)("p-6 pt-0",a.class))},[n(e.$slots,"default")],2))}};export{d as _};
